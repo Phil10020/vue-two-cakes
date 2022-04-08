2a71d108847021ce5c4c35a7fb141c98e6f444e8
Phil <pan10020@gmail.com>
2022-04-09 07:23:44 +0800

    config fresh

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-two-cakes/'
-    : '/'
-}
+export const publicPath = process.env.NODE_ENV === 'production'
+  ? '/vue-two-cakes/'
+  : '/'
